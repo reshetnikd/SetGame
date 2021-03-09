3db69037c99c30da7e2f3cbd397ab01e9e0f20be
reshetnikd <dmitry.reshetnik@icloud.com>
2021-03-09 13:39:21 +0200

    Initial Commit

---
+++ SetGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SetGame
+//
+//  Created by Dmitry Reshetnik on 09.03.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
