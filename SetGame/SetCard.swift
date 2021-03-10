1700ef63499af9049275ee145d2cf8868d3c1468
reshetnikd <dmitry.reshetnik@icloud.com>
2021-03-10 15:25:38 +0200

    Add SetCard structure

---
+++ SetGame/SetCard.swift
@@ -0,0 +1,48 @@
+//
+//  SetCard.swift
+//  SetGame
+//
+//  Created by Dmitry Reshetnik on 10.03.2021.
+//
+
+import Foundation
+
+struct SetCard: Equatable {
+    let number: Int
+    let shape: String
+    let shading: String
+    let color: String
+    
+    enum Number: Int {
+        case one = 1
+        case two = 2
+        case three = 3
+        
+        static var all: [Number] = [Number.one, Number.two, Number.three]
+    }
+    
+    enum Shape: String {
+        case diamond
+        case squiggle
+        case oval
+        
+        static var all: [Shape] = [Shape.diamond, Shape.squiggle, Shape.oval]
+    }
+    
+    enum Shading: String {
+        case striped
+        case solid
+        case outlined
+        
+        static var all: [Shading] = [Shading.striped, Shading.solid, Shading.outlined]
+    }
+    
+    enum Color: String {
+        case red
+        case green
+        case purple
+        
+        static var all: [Color] = [Color.red, Color.green, Color.purple]
+    }
+    
+}
