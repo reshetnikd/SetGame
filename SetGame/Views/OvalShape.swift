15a897e75fbaae8dc1ee01be0a259911b0ee12a6
reshetnikd <dmitry.reshetnik@icloud.com>
2021-03-15 16:35:17 +0200

    Add OvalShape

---
+++ SetGame/Views/OvalShape.swift
@@ -0,0 +1,25 @@
+//
+//  OvalShape.swift
+//  SetGame
+//
+//  Created by Dmitry Reshetnik on 15.03.2021.
+//
+
+import SwiftUI
+
+struct OvalShape: Shape {
+    func path(in rect: CGRect) -> Path {
+        let oval: UIBezierPath = UIBezierPath()
+        oval.addArc(withCenter: CGPoint(x: rect.midX - rect.size.width/4, y: rect.midY), radius: rect.height/2, startAngle: CGFloat.pi/2, endAngle: (3 * CGFloat.pi)/2, clockwise: true)
+        oval.addLine(to: CGPoint(x: rect.midX + rect.size.width/4, y: rect.minY))
+        oval.addArc(withCenter: CGPoint(x: rect.midX + rect.size.width/4, y: rect.midY), radius: rect.height/2, startAngle: (3 * CGFloat.pi)/2, endAngle: CGFloat.pi/2, clockwise: true)
+        oval.close()
+        return Path(oval.cgPath)
+    }
+}
+
+struct OvalShape_Previews: PreviewProvider {
+    static var previews: some View {
+        OvalShape()
+    }
+}
